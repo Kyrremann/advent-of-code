7650153d46c0a296b1c10710b73b5f32f396799c
Kyrre Havik <kyrremann@gmail.com>
2020-12-02 08:52:08 +0100

    day2: solved second puzzle

--- y2020/day-2.rb
+++ y2020/day-2.rb
@@ -27,6 +27,22 @@ def star_1
 end
 
 def star_2
+  correct_passwords_counter = 0
+  PASSWORDS.each do |p|
+    matches = p.match(/(?<first>\d+)-(?<second>\d+)\s(?<letter>\w):\s(?<password>\w+)/)
+    if matches
+      first = matches[:first].to_i - 1
+      second = matches[:second].to_i - 1
+      letter = matches[:letter]
+      password = matches[:password]
+
+      if (password[first] == letter and password[second] != letter) or (password[first] != letter and password[second] == letter)
+        correct_passwords_counter += 1
+      end
+    end
+  end
+
+  p correct_passwords_counter
 end
 
 star_1
