1446e343045ffe76d2cac2fad199edc04a734017
Kyrre Havik <kyrremann@gmail.com>
2020-12-03 09:25:10 +0100

    day3: reused star_1 as base for solving star_2

--- y2020/day-03.rb
+++ y2020/day-03.rb
@@ -12,21 +12,20 @@ def get_location(loc)
   INPUT[loc.y][loc.x]
 end
 
-def move(loc)
-  loc.x += 3
-  loc.y += 1
+def move(loc, slope)
+  loc.x += slope.x
+  loc.y += slope.y
 
   if loc.x >= INPUT[loc.y].length
     loc.x = loc.x - INPUT[loc.y].length
   end
 end
 
-def star_1
+def star_1(slope)
   trees = 0
   me = Location.new(0, 0)
   loop do
-    move(me)
-    p get_location(me)
+    move(me, slope)
     if get_location(me) == TREE
       trees += 1
     end
@@ -34,10 +33,17 @@ def star_1
   end
 
   p "I've seen #{trees} trees"
+  trees
 end
 
 def star_2
+  trees = star_1(Location.new(1,1))
+  trees *= star_1(Location.new(3,1))
+  trees *= star_1(Location.new(5,1))
+  trees *= star_1(Location.new(7,1))
+  trees *= star_1(Location.new(1,2))
+  p "All #{trees} have been seen"
 end
 
-star_1
+star_1(Location.new(3, 1))
 star_2
