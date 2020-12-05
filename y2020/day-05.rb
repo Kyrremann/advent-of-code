1638ab118720834f75c465a5ee57c63e9bff526b
Kyrre Havik <kyrremann@gmail.com>
2020-12-05 11:32:33 +0100

    day5: added two binary solutions

--- y2020/day-05.rb
+++ y2020/day-05.rb
@@ -20,9 +20,26 @@ end
 
 def find_seat(boarding_pass)
   matches = boarding_pass.match(/^(\w{7})(\w{3})$/)
+  to_binary(matches)
+end
+
+## original solution
+def to_range(matches)
   row = parser(matches[1].chars, (0..127).to_a)
   column = parser(matches[2].chars, (0..7).to_a)
   return row, column, (row * 8) + column
+end
+
+## first try on binary solution
+def to_binary(matches)
+  row = matches[1].gsub('F', '0').gsub('B', '1').to_i(2)
+  column = matches[2].gsub('L', '0').gsub('R', '1').to_i(2)
+  return row, column, (row * 8) + column
+end
+
+## if you only need the seat, this is the simplest wasy
+def simple_binary(boarding_pass)
+  boarding_pass.gsub(/[FL]/, '0').gsub(/[BR]/, '1').to_i(2)
 end
 
 def tests
@@ -45,7 +62,7 @@ end
 
 def star_2
   all_seats = (95..838).to_a
-  seats = INPUT.map {|bp| find_seat(bp)[2]}.sort
+  seats = INPUT.map {|bp| simple_binary(bp)}.sort
   p "My seat id: #{(all_seats - seats)}"
 end
 
