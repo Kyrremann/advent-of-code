4fb97d07dcc74cd8eaef9b52c4bf8d273b7cb02e
Kyrre Havik <kyrremann@gmail.com>
2020-12-12 13:29:52 +0100

    day12: star 2 solved, with Waypoint class

--- y2020/day-12.rb
+++ y2020/day-12.rb
@@ -72,6 +72,53 @@ class Boat
   end
 end
 
+class Waypoint
+  include Movable
+
+  def initialize(boat, x, y)
+    @boat = boat
+    @x = x
+    @y = y
+  end
+
+  def direction(input)
+    action, value = [input[0], input[1..-1].to_i]
+    case action
+    when 'F'
+      @boat.move('N',  @y * value)
+      @boat.move('E',  @x * value)
+    when 'L'
+      left(value)
+    when 'R'
+      right(value)
+    else
+      move(action, value)
+    end
+  end
+
+  def left(value)
+    case value
+    when 90
+      @x, @y = [-@y, @x]
+    when 180
+      @x, @y = [-@x, -@y]
+    when 270
+      right(90)
+    end
+  end
+
+  def right(value)
+    case value
+    when 90
+      @x, @y = [@y, -@x]
+    when 180
+      @x, @y = [-@x, -@y]
+    when 270
+      left(90)
+    end
+  end
+end
+
 def star_1
   boat = Boat.new('E', 0, 0)
   TEST_INPUT.each {|i| boat.direction(i)}
@@ -82,6 +129,14 @@ def star_1
 end
 
 def star_2
+  boat = Boat.new('E', 0, 0)
+  waypoint = Waypoint.new(boat, 10, 1)
+  TEST_INPUT.each {|i| waypoint.direction(i)}
+  p "The test boat should move 286: #{boat.count == 286}"
+  boat = Boat.new('E', 0, 0)
+  waypoint = Waypoint.new(boat, 10, 1)
+  INPUT.each {|i| waypoint.direction(i)}
+  p "The boat moved: #{boat.count}"
 end
 
 star_1
