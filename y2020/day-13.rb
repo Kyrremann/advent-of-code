c2bafb9ea68cc099df5a7eee003e08e5f5875a07
Kyrre Havik <kyrremann@gmail.com>
2020-12-13 09:17:54 +0100

    day13: simple solution for star 1

--- y2020/day-13.rb
+++ y2020/day-13.rb
@@ -1,11 +1,24 @@
 #!/usr/bin/env ruby
 
 INPUT = []
-File.readlines('input/day-?.txt').each do |line|
+
+File.readlines('input/day-13.txt').each do |line|
   INPUT << line.chomp
+end
+INPUT[0] = INPUT.first.to_i
+
+TEST_INPUT = [939, [7, 13, 59, 31, 19]]
+
+def bus_calculator(timestamp, input)
+  bus = input.map{|i| t=i; while t < timestamp do; t+=i; end; [i, t]}.min{|a,b| a[1]<=>b[1]}
+  departing_in = bus[1] - timestamp
+  departing_in * bus[0]
 end
 
 def star_1
+  p "Magic test number is 295: #{bus_calculator(TEST_INPUT.first, TEST_INPUT.last) == 295}"
+  input = INPUT.last.split(',').select{|n| n!='x'}.map{|n|n.to_i}
+  p "Magic test number is: #{bus_calculator(INPUT.first, input)}"
 end
 
 def star_2
