a0ebc2a901588b12c468cdeee1ee46374938dfc5
Kyrre Havik <kyrremann@gmail.com>
2020-12-14 21:24:18 +0100

    day14: star 1 modified INPUT data! Fixed

--- y2020/day-14.rb
+++ y2020/day-14.rb
@@ -13,37 +13,26 @@ File.read('input/day-14.txt').lines(chomp: true).each do |line|
   end
 end
 
-TEST_INPUT = [
-  ['XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X', {7 => 101, 8 => 0}],
-  ['000000000000000000000000000000X1001X', {42 => 100}],
-  ['00000000000000000000000000000000X0XX', {26 => 1}]
-]
-
 def bitmask(mask, memory)
-  memory.transform_values! {|v| v.to_s(2).rjust(mask.length, '0').chars.map.with_index {|s,i| mask[i] == 'X' ? s : mask[i]}.join.to_i(2)}
+  memory.transform_values {|v| v.to_s(2).rjust(mask.length, '0').chars.map.with_index {|s,i| mask[i] == 'X' ? s : mask[i]}.join.to_i(2)}
 end
 
 def bitmasker(input)
   bitmasked = input.map do |blob|
     bitmask(blob.first, blob.last)
   end
-
   memory = {}
   bitmasked.each do |bits|
     bits.each do |k,v|
       memory[k] = v
     end
   end
-  memory
-end
-
-def sum_memory(input)
-  bitmasker(input).sum{|_,v| v}
+  memory.sum{|_,v| v}
 end
 
 def star_1
-  p "This is test should sum to 216: #{sum_memory(TEST_INPUT) == 216}"
-  p "The sum of memory is #{sum_memory(INPUT)}"
+  p "This test should sum to 216: #{bitmask('XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X', {7 => 101, 8 => 0}).sum{|_,v| v} == 165}"
+  p "The sum of memory is #{bitmasker(INPUT)}"
 end
 
 def star_2
