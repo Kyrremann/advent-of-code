3f9391597aef9fd7531b74ecf3744a3bd561d964
Kyrre Havik <kyrremann@gmail.com>
2020-12-24 15:56:49 +0100

    day24: not working part 2, something is not flipping

--- y2020/day-24.rb
+++ y2020/day-24.rb
@@ -26,7 +26,10 @@ eneswnwswnwsenenwnwnwwseeswneewsenese
 neswnwewnwnwseenwseesewsenwsweewe
 wseweeenwnesenwwwswnew)
 
+BLACK = 1
+WHITE = 0
 COORDS = {"e" => [+1,0], "se" => [0,+1], "sw" => [-1,+1], "w" => [-1,0], "nw" => [0,-1], "ne" => [+1,-1]}
+
 def move_to(steps)
   person = [0,0]
   steps = steps.chars
@@ -39,18 +42,58 @@ def move_to(steps)
   person
 end
 
-def count_black_tiles(input)
+def black?(n)
+  n == BLACK
+end
+
+def get_movements(input)
   map = Hash.new(0)
-  input.map {|cords| move_to(cords)}.each {|m| map[m] += 1}
-  map.values.count{ |x| x%2!=0 }
+  input.map {|cords| move_to(cords)}.each {|k| map[k] = black?(map[k]) ? WHITE : BLACK }
+  map
+end
+
+def count_black_tiles(map)
+  map.values.count{ |x| black?(x) }
 end
 
 def star_1
-  p "There should be 10 black tiles: #{count_black_tiles(TEST_INPUT) == 10}"
-  p "There are #{count_black_tiles(INPUT)} black tiles"
+  p "There should be 10 black tiles: #{count_black_tiles(get_movements(TEST_INPUT)) == 10}"
+  p "There are #{count_black_tiles(get_movements(INPUT))} black tiles"
+end
+
+def add_surrounding_tiles(tiles)
+  tiles.each do |coord,v|
+    COORDS.values.each {|c| tiles[[coord[0]+c[0], coord[1]+c[1]]] ||= 0 }
+  end
+end
+
+def count_surrounding_black_tiles(tiles, coord)
+  COORDS.values.count {|v| black?(tiles[[coord[0]+v[0], coord[1]+v[1]]])}
+end
+
+def flip_tile(tiles, tmp, tile, tile_color)
+  blacks = count_surrounding_black_tiles(tmp, tile)
+  if black?(tile_color)
+    tiles[tile] = WHITE if blacks == 0 || blacks > 2
+  else
+    tiles[tile] = BLACK if blacks == 2
+  end
+end
+
+def flip(tiles)
+  tmp = tiles.clone
+  tmp.each do |tile, color|
+    coords = COORDS.map {|k,c| [tile[0]+c[0], tile[1]+c[1]]}.append(tile)
+    coords.each {|coord| flip_tile(tiles, tmp, coord, color)}
+  end
+  tiles
 end
 
 def star_2
+  tiles = get_movements(TEST_INPUT)
+  p ["tiles", tiles.count, "black", count_black_tiles(tiles)]
+  tiles = flip(tiles)
+  p ["tiles", tiles.count, "black", count_black_tiles(tiles)]
 end
 
 star_1
