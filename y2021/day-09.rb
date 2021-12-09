9aa4ecbcad0b6f3ada532db535a133dcf851d534
Kyrre Havik <kyrremann@gmail.com>
2021-12-09 14:17:16 +0100

    day9: finished star2
    with some refactoring of star1

--- y2021/day-09.rb
+++ y2021/day-09.rb
@@ -13,19 +13,23 @@ test = ['2199943210',
 def around(input, x, y)
   values = []
 
-  values << input[y-1][x] if y > 0
-  values << input[y][x+1] if x+1 < input[y].size
-  values << input[y][x-1] if x > 0
-  values << input[y+1][x] if y+1 < input.size
+  values << [x+1, y] if x+1 < input[y].size
+  values << [x, y+1] if y+1 < input.size
+  values << [x-1, y] if x > 0
+  values << [x, y-1] if y > 0
   values
 end
 
-def explore(input)
+def around_depth(input, x, y)
+  around(input, x, y).map {|loc| x,y = loc; input[y][x].to_i}
+end
+
+def basin(input)
   locations = []
   input.each_with_index do |row, y|
     row.chars.each_with_index do |column, x|
-      values = around(input, x, y)
-      locations << [x,y] if values.all? {|n| n > column }
+      values = around_depth(input, x, y)
+      locations << [x,y] if values.all? {|n| n > column.to_i }
     end
   end
 
@@ -33,8 +37,23 @@ def explore(input)
 end
 
 def star1(input)
-  explore(input).map {|loc| x,y = loc; input[y][x].to_i}.sum {|n| n+1}
+  basin(input).map {|loc| x,y = loc; input[y][x].to_i}.sum {|n| n+1}
 end
 
-p star1(test)
-p star1(INPUT)
+p "TEST: Risk level: #{star1(test)}"
+p "Risk level: #{star1(INPUT)}"
+
+def explore(input, x, y)
+  basin = input[y][x].to_i
+  locations = around(input, x, y)
+  new_locations = locations.select {|loc| _x,_y = loc; depth = input[_y][_x].to_i; depth > basin && depth < 9}
+  [[x,y]] + new_locations.map {|loc| explore(input, *loc)}.flatten(1)
+end  
+
+def star2(input)
+  bottom = basin(input)
+  bottom.map {|loc| explore(input, *loc).uniq.count}.max(3).inject(:*)
+end
+
+p "TEST: Basin size: #{star2(test)}"
+p "Basin size: #{star2(INPUT)}"
