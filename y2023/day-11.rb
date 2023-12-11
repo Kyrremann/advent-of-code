da7a02b12203e9550b41fc18e47fc5cef95b7326
Kyrre Havik <kyrremann@gmail.com>
2023-12-12 00:51:01 +0100

    day11: star 2

--- y2023/day-11.rb
+++ y2023/day-11.rb
@@ -20,7 +20,7 @@ def expand_space(space)
   space_to_add = []
 
   space.each_with_index do |line, index|
-    space_to_add << index if line.all? { |area| area == '.' }
+    space_to_add << index unless line.any? { |area| area == '#' }
   end
 
   space_to_add.each_with_index do |index, i|
@@ -29,7 +29,7 @@ def expand_space(space)
 
   space_to_add = []
   space.first.size.times do |index|
-    space_to_add << index if space.all? { |line| line[index] == '.' }
+    space_to_add << index unless space.any? { |line| line[index] == '#' }
   end
 
   space_to_add.each_with_index do |index, i|
@@ -75,10 +75,53 @@ p "Test: #{star1(test_input)} == 374"
 print 'Star 1: '
 p star1(INPUT)
 
-def star2(input)
+def expand_old_space(space)
+  y = []
+  space.each_with_index do |line, index|
+    y << index unless line.any? { |area| area == '#' }
+  end
+
+  x = []
+  space.first.size.times do |index|
+    x << index unless space.any? { |line| line[index] == '#' }
+  end
+
+  [x.sort, y.sort]
 end
 
-p "Test: #{star2(test_input)} == 46"
+# x, y
+def distance_old(start, finish, expanded_x, expanded_y)
+  size = 1_000_000
+  shortest = (start[0] - finish[0]).abs + (start[1] - finish[1]).abs
+
+  direction_x = [start[0], finish[0]].sort
+  expanded_space_x = expanded_x.select { |x| x > direction_x.first && x < direction_x.last }
+
+  direction_y = [start[1], finish[1]].sort
+  expanded_space_y = expanded_y.select { |y| y > direction_y.first && y < direction_y.last }
+
+  shortest +
+    (expanded_space_y.size * size) - expanded_space_y.size +
+    (expanded_space_x.size * size) - expanded_space_x.size
+end
+
+def find_shortest_path_in_old_space(start, galaxies, expanded_x, expanded_y)
+  return 0 if galaxies.nil? || galaxies.empty?
+
+  galaxies.sum { |galaxy| distance_old(start, galaxy, expanded_x, expanded_y) }
+end
+
+def star2(input)
+  space = input.split("\n").map(&:chars)
+  expanded_x, expanded_y = expand_old_space(space)
+
+  galaxies = find_galaxies(space)
+  galaxies.each_with_index.sum do |galaxy, index|
+    find_shortest_path_in_old_space(galaxy, galaxies[(index + 1)...], expanded_x, expanded_y)
+  end
+end
+
+p "Test: #{star2(test_input)} == ???"
 
 print 'Star 2: '
 p star2(INPUT)
