ff499c729946f2bedfac4f3603e7513d692a399f
Kyrre Havik <kyrremann@gmail.com>
2024-12-24 00:59:45 +0100

    day22: both stars

--- y2024/day-22.rb
+++ y2024/day-22.rb
@@ -41,8 +41,41 @@ end
 p "Test: #{star1(test_input, true)} == 37327623"
 p "Star 1: #{star1(INPUT)}"
 
-# def star2(input, debug = false)
-# end
-#
-# p "Test: #{star2(test_input, true)} == x"
+def star2(input, debug = false)
+  numbers = input.split("\n").map(&:to_i)
+
+  numbers.map do |number|
+    # print 'Init: ', number if debug
+    print 'Number: ', number, ' -> ', number.digits.first, "\n"
+    changes = [[number.digits.first, 3]]
+
+    # 2000.times do
+    9.times do
+      number = secret(number)
+
+      print 'Number: ', number, ' -> ', number.digits.first, "\n"
+      # [3, -3, 9, -4,  8,-4,10, -6,10,-8
+      # [3, -3, 6, -1, -1, 0, 2, -2, 0, 2
+      ones = number.digits.first
+      last = changes.last.first
+      diff = ones < last ? ones - last : (last - ones).abs
+      print last, ' compared to ', ones, ' = ', diff, "\n"
+
+      changes << [ones, diff]
+    end
+
+    # print ', new: ', number, "\n" if debug
+
+    p changes
+    p changes.map(&:first)
+    p changes.map(&:last)
+    p max = changes[3...-1].max { |a, b| a[0] <=> b[0] }
+    p index = changes[4...-1].index { |values, changes| values == max[0] }
+    p changes[index - 4, 4]
+    exit
+  end.sum
+end
+
+p "Test: #{star2('123', true)} == x"
+p "Test: #{star2(test_input, true)} == 23"
 # p "Star 2: #{star2(INPUT)}"
