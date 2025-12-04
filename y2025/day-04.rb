58ab7f1a95be8a0c32284b487b9dd7b95e776a5e
Kyrre Havik <kyrre@tuta.com>
2025-12-04 21:42:27 +0100

    day04: second star

--- y2025/day-04.rb
+++ y2025/day-04.rb
@@ -49,8 +49,51 @@ end
 p "Test: #{star1(test_input, debug: false)} == 13"
 p "Star 1: #{star1(INPUT)}"
 
-# def star2(input, debug: false)
-# end
-#
-# p "Test: #{star2(test_input, debug: true)} == x"
-# p "Star 2: #{star2(INPUT)}"
+def find_roll(map, row, col)
+  directions = [
+    [-1, -1], [-1, 0], [-1, 1],
+    [ 0, -1],          [ 0, 1],
+    [ 1, -1], [ 1, 0], [ 1, 1]
+  ]
+
+  directions.select do |dy, dx|
+    y = dy + row
+    x = dx + col
+
+    y.between?(0, map.size - 1) &&
+                     x.between?(0, map[0].size - 1) &&
+                     map[y][x] == '@'
+  end
+end
+
+def count_and_ship(map)
+  ship_it = []
+  (0...map.length).each do |y|
+    (0...map[y].length).each do |x|
+      next if map[y][x] == '.' || too_many_rolls(map, y, x).count > 3
+
+      ship_it << [y, x]
+    end
+  end
+
+  ship_it
+end
+
+def star2(input, debug: false)
+  map = input.split.map(&:chars)
+  count = 0
+  loop do
+    ship_it = count_and_ship(map)
+    count += ship_it.size
+    ship_it.each do |y, x|
+      map[y][x] = '.'
+    end
+
+    break if ship_it.empty?
+  end
+
+  count
+end
+
+p "Test: #{star2(test_input, debug: true)} == 43"
+p "Star 2: #{star2(INPUT)}"
