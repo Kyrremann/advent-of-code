1926636cd548fd5eba8a25cd416c311403bceb38
Kyrre Havik <kyrre@tuta.com>
2025-12-05 16:11:42 +0100

    day03: not working

--- y2025/day-03.rb
+++ y2025/day-03.rb
@@ -11,37 +11,53 @@ test_input = '987654321111111
 818181911112111'
 
 def max(array, history)
-  max = -1
+  max = []
   array.each_with_index do |v, i|
     next if history.include?(i)
 
-    max = [v, i] if v > max[0]
+    max = [v, i] if v > (max.first || -1)
   end
 
   max
 end
 
+def solve(chars, history: [])
+  m = max(chars, history)
+  m2 = max(chars[m.last + 1..], [])
+
+  if m2.empty?
+    history << m.last
+    return solve(chars, history: history)
+  end
+
+  [m, m2]
+end
+
 def star1(input, debug: false)
-  history = []
-  input.split.map do |line|
-    chars = line.chars.map(&:to_i)
-    m1 = max(chars, history)
-    m2 = chars[m1.last + 1..].max
-
-    if m2.nil?
-      history << m1.last
-      redo
-    end
-
-    [m1.first, m2].join.to_i
+  input.split.map do |fresh|
+    solve(fresh.chars.map(&:to_i)).map(&:first).join.to_i
   end.sum
 end
 
 p "Test: #{star1(test_input, debug: true)} == 357"
 p "Star 1: #{star1(INPUT)}"
 
-# def star2(input, debug: false)
-# end
+def solve(chars, history, place)
+  return '' if place < 1
 
-# p "Test: #{star2(test_input, debug: true)} == x"
+  m = max(chars, history)
+  history << m.last
+  p m
+  place -= 1 unless m.last > chars.length - place
+  m.first.to_s + solve(chars, history, place) # if m1.last > chars.length - place)
+end
+
+def star2(input, debug: false)
+  input.split.map do |fresh|
+    chars = fresh.chars.map(&:to_i)
+    p solve(chars, [], 12).to_i # .map(&:first).join.to_i
+  end.sum
+end
+
+p "Test: #{star2(test_input, debug: true)} == 3121910778619"
 # p "Star 2: #{star2(INPUT)}"
