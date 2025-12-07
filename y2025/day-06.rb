519f1f42740a1f3494e84f2dcc116c32a4e24b73
Kyrre Havik <kyrre@tuta.com>
2025-12-07 10:14:27 +0100

    day06: started thinking on part 2

--- y2025/day-06.rb
+++ y2025/day-06.rb
@@ -5,40 +5,68 @@ input_file = "input/#{File.basename(__FILE__).match(/\d\d?/)[0]}.txt"
 
 INPUT = File.read(input_file) if File.exist?(input_file)
 
-test_input = '123 328  51 64
- 45 64  387 23
+test_input = '123 328  51 64 
+ 45 64  387 23 
   6 98  215 314
-*   +   *   +'
+*   +   *   +  '
 
-def star1(input, debug: false)
+def columnizer(input)
   problems = []
+  operators = []
   input.split("\n") do |line|
     line.split.each_with_index do |number, x|
       problems[x] ||= []
 
       if ['*', '+'].include?(number)
-        problems[x] << number
+        operators[x] = number
       else
         problems[x] << number.to_i
       end
     end
   end
-  problems.sum do |problem|
-    operator = problem.pop
-    case operator
-    when '*'
-      problem.inject(:*)
+
+  [problems, operators]
+end
+
+def star1(input, debug: false)
+  problems, operators = columnizer(input)
+  sum = 0
+  operators.each_with_index do |operator, index|
+    sum += case operator
+    when "*"
+      problems[index].inject(:*)
     when '+'
-      problem.sum
+      problems[index].sum
     end
   end
+
+  sum
 end
 
 p "Test: #{star1(test_input, debug: true)} == 4277556"
 p "Star 1: #{star1(INPUT)}"
 
-# def star2(input, debug = false)
-# end
+# 123
+#  45
+#   6
 #
-# p "Test: #{star2(test_input, true)} == x"
+# 356
+#  24
+#   1
+
+def star2(input, debug = false)
+  problems, operators = columnizer(input)
+  problems.each do |problem|
+    modified = problem.map(&:to_s).map(&:chars)
+
+    maths = []
+    (0...problem.size).each do |i|
+      maths[i] = modified.map(&:pop).join.to_i
+    end
+
+    p maths
+  end
+end
+
+p "Test: #{star2(test_input, true)} == 3263827"
 # p "Star 2: #{star2(INPUT)}"
